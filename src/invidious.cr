7fc7a47849896aeb49aa8c734f9c4eb1f4b9d907
Omar Roth <omarroth@hotmail.com>
2017-12-30 15:31:01 -0600

    Remove unused info

--- src/invidious.cr
+++ src/invidious.cr
@@ -5,116 +5,6 @@ require "pg"
 require "xml"
 require "time"
 
-# class AdaptiveFmts
-#   JSON.mapping(
-#     clen: Int32,
-#     url: String,
-#     lmt: Int64,
-#     index: String,
-#     fps: Int32,
-#     itag: Int32,
-#     projection_type: Int32,
-#     size: String,
-#     init: String,
-#     quality_label: String,
-#     bitrate: Int32,
-#     type: String
-#   )
-# end
-
-# class URLEncodedFmtStreamMap
-#   JSON.mapping(
-#     url: String,
-#     itag: Int32,
-#     fallback_host: String,
-#     quality: String,
-#     type: String
-#   )
-# end
-
-class CaptionTranslationLanguages
-  JSON.mapping(
-    lc: String,
-    n: String
-  )
-end
-
-class VideoInfo
-  JSON.mapping(
-    cver: String,
-    length_seconds: Int32,
-    iurlhq720: String,
-    vm: String,
-    ypc_ad_indicator: Int32,
-    hash_cc: Bool,
-    dashmpd: String,
-    iv3_module: Int32,
-    iurlmq: String,
-    no_get_video_log: Int32,
-    cc_font: Int32,
-    allowed_ads: String,
-    oid: String,
-    iv_invideo_url: String,
-    cc_asr: Int32,
-    relative_loudness: Float64,
-    video_verticals: String,
-    default_audio_track_index: Int32,
-    loudness: Float64,
-    ptchn: String,
-    csn: String,
-    pltype: String,
-    author: String,
-    # caption_audio_tracks:
-    videostats_playback_base_url: String,
-    root_ve_type: String,
-    muted: Int32,
-    cc3_module: Int32,
-    adaptive_fmts: AdaptiveFmts,
-    fmt_list: Array(String),
-    allow_embed: Int32,
-    iurlhq: String,
-    use_cipher_signature: Bool,
-    status: String,
-    video_id: String,
-    idpj: Int32,
-    iurlhmaxres: String,
-    short_view_count_text: String,
-    iv_load_policy: Int32,
-    plid: String,
-    vss_host: String,
-    ttsurl: String,
-    token: String,
-    account_playback_token: String,
-    of: String,
-    iurl: String,
-    iurlsd: String,
-    c: String,
-    timestamp: Int32,
-    url_encoded_fmt_stream_map: URLEncodedFmtStreamMap,
-    allow_ratings: Int32,
-    view_count: Int64,
-    title: String,
-    caption_tracks: CaptionTracks,
-    fexp: Array(String),
-    storyboard_spec: String,
-    keywords: Array(String),
-    ucid: String,
-    remarketing_url: String,
-    caption_translation_languages: CaptionTranslationLanguages,
-    avg_rating: Float64,
-    is_listed: Int32,
-    ptk: String,
-    cl: Int32,
-    watermark: Array(String),
-    ldpj: Int32,
-    tmi: Int32,
-    eventid: String,
-    thumbnail_url: String
-  )
-end
-
-class Record
-end
 
 macro templated(filename)
   render "src/views/#{{{filename}}}.ecr", "src/views/layout.ecr"
@@ -226,8 +116,8 @@ get "/watch" do |env|
       video_record = get_video(video_id, context)
       pg.exec("update videos set last_updated = $1, video_info = $3, video_html = $4,\
       views = $5, likes = $6, dislikes = $7, rating = $8, description = $9 where video_id = $2",
-      video_record.to_a)
-  end
+        video_record.to_a)
+    end
   else
     client = HTTP::Client.new("www.youtube.com", 443, context)
     video_info = client.get("/get_video_info?video_id=#{video_id}&el=info&ps=default&eurl=&gl=US&hl=en").body
