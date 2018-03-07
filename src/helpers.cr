212ba0504795da802b42083c8be469dc7ce326e3
Omar Roth <omarroth@hotmail.com>
2018-03-07 08:07:40 -0600

    Update signature function

--- src/helpers.cr
+++ src/helpers.cr
@@ -206,16 +206,13 @@ end
 def decrypt_signature(a)
   a = a.split("")
 
-  # a.reverse!
-  # a.delete_at(0..2)
-  # a = splice(a, 35)
-  # a.delete_at(0)
-  # a.reverse!
-  # a = splice(a, 54)
-
-  a = splice(a, 53)
-  a = splice(a, 47)
+  a.reverse!
+  a = splice(a, 23)
   a.delete_at(0..2)
+  a.reverse!
+  a = splice(a, 45)
+  a.reverse!
+  a = splice(a, 66)
 
   return a.join("")
 end
