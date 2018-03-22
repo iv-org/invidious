228008168a83e01bb3dfe602d6df24c5a31a26c2
Omar Roth <omarroth@hotmail.com>
2018-03-22 11:02:15 -0500

    Update signature

--- src/helpers.cr
+++ src/helpers.cr
@@ -223,10 +223,15 @@ end
 def decrypt_signature(a)
   a = a.split("")
 
+  a = splice(a, 12)
+  a = splice(a, 18)
   a.reverse!
-  a.delete_at(0)
+  a.delete_at(0..2)
   a.reverse!
-  a = splice(a, 50)
+  a.delete_at(0..2)
+  a = splice(a, 69)
+  a.reverse!
+  a.delete_at(0..2)
 
   return a.join("")
 end
