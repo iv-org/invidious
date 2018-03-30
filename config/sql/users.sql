0aa8ad9a7cc8581269cb5d5b44827951ee2369c3
Omar Roth <omarroth@hotmail.com>
2018-03-29 22:27:51 -0500

    Make user id unique

--- config/sql/users.sql
+++ config/sql/users.sql
@@ -7,7 +7,8 @@ CREATE TABLE public.users
     id text COLLATE pg_catalog."default" NOT NULL,
     updated timestamp with time zone,
     notifications integer,
-    subscriptions text[] COLLATE pg_catalog."default"
+    subscriptions text[] COLLATE pg_catalog."default",
+    CONSTRAINT users_id_key UNIQUE (id)
 )
 WITH (
     OIDS = FALSE
