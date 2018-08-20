deb4b06ea0ebfdc629c1c54c920eeb147b4f91d3
Omar Roth <omarroth@hotmail.com>
2018-08-20 10:25:05 -0500

    Fix playlist view extractor

--- src/invidious/playlists.cr
+++ src/invidious/playlists.cr
@@ -139,8 +139,8 @@ def fetch_playlist(plid)
   author = anchor.xpath_node(%q(.//li[1]/a)).not_nil!.content
   ucid = anchor.xpath_node(%q(.//li[1]/a)).not_nil!["href"].split("/")[2]
 
-  video_count = anchor.xpath_node(%q(.//li[2])).not_nil!.content.delete("videos").to_i
-  views = anchor.xpath_node(%q(.//li[3])).not_nil!.content.delete("views,").to_i64
+  video_count = anchor.xpath_node(%q(.//li[2])).not_nil!.content.delete("videos, ").to_i
+  views = anchor.xpath_node(%q(.//li[3])).not_nil!.content.delete("views, ").to_i64
 
   updated = anchor.xpath_node(%q(.//li[4])).not_nil!.content.lchop("Last updated on ").lchop("Updated ")
   updated = decode_date(updated)
