8fd03e76214eb2df88acfb1b4523375b40a3f76f
Omar Roth <omarroth@hotmail.com>
2018-10-31 11:07:58 -0500

    Only expose API

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-createdb invidious
-#createuser kemal
-psql -c "CREATE USER kemal WITH PASSWORD 'kemal';"
-psql invidious < config/sql/channels.sql
-psql invidious < config/sql/videos.sql
-psql invidious < config/sql/channel_videos.sql
-psql invidious < config/sql/users.sql
