629b0a8b4105728e201f5a6edb1b1120641521b8
Perflyst <mail@perflyst.de>
2019-01-21 19:17:07 +0100

    Use random password for database user
    Fix #298

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+dbpass=$(openssl rand -hex 20)
+
 createdb invidious
 #createuser kemal
-psql -c "CREATE USER kemal WITH PASSWORD 'kemal';"
+psql -c "CREATE USER kemal WITH PASSWORD '$dbpass';"
 psql invidious < config/sql/channels.sql
 psql invidious < config/sql/videos.sql
 psql invidious < config/sql/channel_videos.sql
 psql invidious < config/sql/users.sql
 psql invidious < config/sql/nonces.sql
+
+# change password in config file
+sed -i "s/replacethispassword/$dbpass/g" config/config.yml
