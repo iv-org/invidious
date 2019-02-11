52cad8d6daa95a1fdf9d2f3cb44c72b14b4a5520
Omar Roth <omarroth@hotmail.com>
2019-02-11 10:59:17 -0600

    Update change index for channel_videos and add index for nonces

--- config/sql/nonces.sql
+++ config/sql/nonces.sql
@@ -5,10 +5,18 @@
 CREATE TABLE public.nonces
 (
   nonce text,
-  expire timestamp with time zone
-)
-WITH (
-  OIDS=FALSE
+  expire timestamp with time zone,
+  CONSTRAINT nonces_id_key UNIQUE (nonce)
 );
 
 GRANT ALL ON TABLE public.nonces TO kemal;
+
+-- Index: public.nonces_nonce_idx
+
+-- DROP INDEX public.nonces_nonce_idx;
+
+CREATE INDEX nonces_nonce_idx
+  ON public.nonces
+  USING btree
+  (nonce COLLATE pg_catalog."default");
+
