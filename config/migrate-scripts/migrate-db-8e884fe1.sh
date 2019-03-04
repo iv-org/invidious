90715467a245d08441b8454d0f4dfa5295038306
Omar Roth <omarroth@hotmail.com>
2019-03-03 20:44:29 -0600

    Set default value for 'subscribed' date

--- config/migrate-scripts/migrate-db-8e884fe1.sh
+++ config/migrate-scripts/migrate-db-8e884fe1.sh
@@ -2,3 +2,4 @@
 
 psql invidious -c "ALTER TABLE channels DROP COLUMN subscribed"
 psql invidious -c "ALTER TABLE channels ADD COLUMN subscribed timestamptz"
+psql invidious -c "UPDATE channels SET subscribed = '2019-01-01 00:00:00+00'"
