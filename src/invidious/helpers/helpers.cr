21190a240fcd3b280e0d5be70bbd85fafbc60800
Omar Roth <omarroth@protonmail.com>
2019-04-30 21:17:34 -0500

    Add support for adding banner to site header

--- src/invidious/helpers/helpers.cr
+++ src/invidious/helpers/helpers.cr
@@ -125,6 +125,7 @@ user: String,
     dmca_content:      {type: Array(String), default: [] of String}, # For compliance with DMCA, disables download widget using list of video IDs
     check_tables:      {type: Bool, default: false},                 # Check table integrity, automatically try to add any missing columns, create tables, etc.
     cache_annotations: {type: Bool, default: false},                 # Cache annotations requested from IA, will not cache empty annotations or annotations that only contain cards
+    banner:            {type: String?, default: nil},                # Optional banner to be displayed along top of page for announcements, etc.
   })
 end
 
