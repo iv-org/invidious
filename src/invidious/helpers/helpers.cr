34c43b83496a525560b589754a386905d9371782
Omar Roth <omarroth@protonmail.com>
2019-09-12 13:06:27 -0400

    Add support for abbreviated sub count in search

--- src/invidious/helpers/helpers.cr
+++ src/invidious/helpers/helpers.cr
@@ -415,7 +415,12 @@ def extract_items(nodeset, ucid = nil, author_name = nil)
 
       author_thumbnail ||= ""
 
-      subscriber_count = node.xpath_node(%q(.//span[contains(@class, "yt-subscriber-count")])).try &.["title"].gsub(/\D/, "").to_i?
+      subscriber_count_text = node.xpath_node(%q(.//span[contains(@class, "yt-subscriber-count")])).try &.["title"]
+      begin
+        subscriber_count = subscriber_count_text.try { |text| short_text_to_number(text) }
+      rescue ex
+        subscriber_count = subscriber_count_text.try &.gsub(/\D/, "").to_i?
+      end
       subscriber_count ||= 0
 
       video_count = node.xpath_node(%q(.//ul[@class="yt-lockup-meta-info"]/li)).try &.content.split(" ")[0].gsub(/\D/, "").to_i?
