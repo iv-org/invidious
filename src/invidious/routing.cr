192d2b86b61f2212d1ae5423eba033deaf386c01
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2020-11-19 19:21:48 -0600

    Extract playlist routes from global file

--- src/invidious/routing.cr
+++ src/invidious/routing.cr
@@ -1,8 +1,15 @@
 module Invidious::Routing
-  macro get(path, controller)
+  macro get(path, controller, method = :handle)
     get {{ path }} do |env|
       controller_instance = {{ controller }}.new(config, logger)
-      controller_instance.handle(env)
+      controller_instance.{{ method.id }}(env)
+    end
+  end
+
+  macro post(path, controller, method = :handle)
+    post {{ path }} do |env|
+      controller_instance = {{ controller }}.new(config, logger)
+      controller_instance.{{ method.id }}(env)
     end
   end
 end
