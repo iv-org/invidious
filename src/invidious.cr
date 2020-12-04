adccca366e12a32fd96859d6f279b83a25f02479
saltycrys <73420320+saltycrys@users.noreply.github.com>
2020-12-04 01:58:27 +0100

    Fix invidious version for old git versions
    The `%cs` format was only added to git in version 2.25 while `%ci` has been
    around forever.

--- src/invidious.cr
+++ src/invidious.cr
@@ -64,7 +64,7 @@ HTTP_CHUNK_SIZE            = 10485760 # ~10MB
 
 CURRENT_BRANCH  = {{ "#{`git branch | sed -n '/* /s///p'`.strip}" }}
 CURRENT_COMMIT  = {{ "#{`git rev-list HEAD --max-count=1 --abbrev-commit`.strip}" }}
-CURRENT_VERSION = {{ "#{`git log -1 --format=%cs | sed s/-/./g`.strip}" }}
+CURRENT_VERSION = {{ "#{`git log -1 --format=%ci | date -I -f - | sed s/-/./g`.strip}" }}
 
 # This is used to determine the `?v=` on the end of file URLs (for cache busting). We
 # only need to expire modified assets, so we can use this to find the last commit that changes
