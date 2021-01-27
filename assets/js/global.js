4a0b10984ad4151a8f7c8b0a9db3ed378a6df57e
Andrew Zhao <azhao12345@users.noreply.github.com>
2021-01-27 10:45:03 -0500

    Bump videojs and fix webworker

--- assets/js/global.js
+++
@@ -1,3 +0,0 @@
-// Disable Web Workers. Fixes Video.js CSP violation (created by `new Worker(objURL)`):
-// Refused to create a worker from 'blob:http://host/id' because it violates the following Content Security Policy directive: "worker-src 'self'".
-window.Worker = undefined;
