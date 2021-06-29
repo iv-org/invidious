44ab9371dcc5666a0fa33ac1f7ec7b5dec9db451
syeopite <syeopite@syeopite.dev>
2021-06-28 23:11:04 -0700

    Manually extract category refactor from 1b569bbc99207cae7c20aa285f42477ae361dd30
    Also fixes some errors caused by cherry-picking

--- src/invidious/helpers/serialized_yt_data.cr
+++ src/invidious/helpers/serialized_yt_data.cr
@@ -232,8 +232,8 @@ class Category
   include DB::Serializable
 
   property title : String
-  property contents : Array(SearchItem)
-  property browse_endpoint_data : String?
+  property contents : Array(SearchItem) | Array(Video)
+  property url : String?
   property description_html : String
   property badges : Array(Tuple(String, String))?
 
