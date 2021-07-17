71ec829c335827777b4bc0411b14f8f706c86724
syeopite <syeopite@syeopite.dev>
2021-07-16 22:44:24 -0700

    Move hardcoded user agents to user-agents.json

--- src/invidious/helpers/random-user-agents.cr
+++ src/invidious/helpers/random-user-agents.cr
@@ -1,26 +1,9 @@
-# Using different browsers would allow us to disguise our traffic even more.
-# However, due to the widely different version and operating system values as well as
-# their frequency on different OSes we're going to need a separate dataset
-# for everything.
-
-AGENT_COMPONENTS = {
-  "safari" => {
-    "os"       => ["Macintosh; Intel Mac OS X 10_15_7", "Macintosh; Intel Mac OS X 10_15_6"],
-    "versions" => ["14.1.1", "14.1", "14.0.3"],
-  },
-
-  "firefox" => {
-    "os" => ["Macintosh; Intel Mac OS X 10.15'", "Macintosh; Intel Mac OS X 10.14",
-             "Windows NT 10.0; Win64; x64", "X11; Ubuntu; Linux x86_64",
-             "X11; Linux x86_64"],
-    "versions" => ["88.0 ", "89.0"],
-  },
-}
+AGENT_COMPONENTS = JSON.parse(File.read("config/user-agents.json")).as_h
 
 private def generate_user_agent
   agent_component = AGENT_COMPONENTS.keys.sample(1)[0]
-  os = AGENT_COMPONENTS[agent_component]["os"].sample(1)[0]
-  version = AGENT_COMPONENTS[agent_component]["versions"].sample(1)[0]
+  os = AGENT_COMPONENTS[agent_component]["os"].as_a.sample(1)[0]
+  version = AGENT_COMPONENTS[agent_component]["versions"].as_a.sample(1)[0]
   base = "Mozilla/5.0 "
 
   case agent_component
