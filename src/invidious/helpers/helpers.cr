4b46313e19f06f2739ee522eb4fbfe8dbcbee630
Samantaz Fox <coding@samantaz.fr>
2021-08-03 23:44:47 +0200

    Fix crystal overrides (#2295)
    * Move Crystal stdlib classes overrides to a separate file
    * Document known crystal overrides
    * Update crystal overrides for HTTP::Client socket
    * Update shard.yml to restrict crystal versions
    * Fix compilation error in Crystal 1.1.x (See
       https://github.com/crystal-lang/crystal/issues/10965
       for more details about this issue).

--- src/invidious/helpers/helpers.cr
+++ src/invidious/helpers/helpers.cr
@@ -509,12 +509,6 @@ def check_table(db, table_name, struct_type = nil)
   end
 end
 
-class PG::ResultSet
-  def field(index = @column_index)
-    @fields.not_nil![index]
-  end
-end
-
 def get_column_array(db, table_name)
   column_array = [] of String
   db.query("SELECT * FROM #{table_name} LIMIT 0") do |rs|
@@ -697,49 +691,5 @@ def proxy_file(response, env)
     end
   else
     IO.copy response.body_io, env.response
-  end
-end
-
-class HTTP::Server::Response
-  class Output
-    private def unbuffered_flush
-      @io.flush
-    rescue ex : IO::Error
-      unbuffered_close
-    end
-  end
-end
-
-class HTTP::Client
-  property family : Socket::Family = Socket::Family::UNSPEC
-
-  private def socket
-    socket = @socket
-    return socket if socket
-
-    hostname = @host.starts_with?('[') && @host.ends_with?(']') ? @host[1..-2] : @host
-    socket = TCPSocket.new hostname, @port, @dns_timeout, @connect_timeout, @family
-    socket.read_timeout = @read_timeout if @read_timeout
-    socket.sync = false
-
-    {% if !flag?(:without_openssl) %}
-      if tls = @tls
-        socket = OpenSSL::SSL::Socket::Client.new(socket, context: tls, sync_close: true, hostname: @host)
-      end
-    {% end %}
-
-    @socket = socket
-  end
-end
-
-class TCPSocket
-  def initialize(host, port, dns_timeout = nil, connect_timeout = nil, family = Socket::Family::UNSPEC)
-    Addrinfo.tcp(host, port, timeout: dns_timeout, family: family) do |addrinfo|
-      super(addrinfo.family, addrinfo.type, addrinfo.protocol)
-      connect(addrinfo, timeout: connect_timeout) do |error|
-        close
-        error
-      end
-    end
   end
 end
