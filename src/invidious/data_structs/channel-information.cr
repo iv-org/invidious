3c01bbb0b324233cdd02322db3c3181c59cff904
syeopite <syeopite@syeopite.dev>
2021-08-06 00:31:06 -0700

    Initial extraction for complete struct overhaul

---
+++ src/invidious/data_structs/channel-information.cr
@@ -0,0 +1,36 @@
+module YTStructs
+  # Struct to represent channel heading information.
+  #
+  # As of master this is mostly taken from the about tab.
+  #
+  # TODO: Refactor into into ChannelInformation
+  struct AboutChannel
+    include DB::Serializable
+
+    property ucid : String
+    property author : String
+    property auto_generated : Bool
+    property author_url : String
+    property author_thumbnail : String
+    property banner : String?
+    property description_html : String
+    property paid : Bool
+    property total_views : Int64
+    property sub_count : Int32
+    property joined : Time
+    property is_family_friendly : Bool
+    property allowed_regions : Array(String)
+    property related_channels : Array(AboutRelatedChannel)
+    property tabs : Array(String)
+  end
+
+  # TODO this should be removed. YouTube has removed related channels.
+  struct AboutRelatedChannel
+    include DB::Serializable
+
+    property ucid : String
+    property author : String
+    property author_url : String
+    property author_thumbnail : String
+  end
+end
