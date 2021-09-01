d4ae0a47dbedd9c17607df314b7a508eb433136c
Samantaz Fox <coding@samantaz.fr>
2021-09-02 00:00:07 +0200

    Remove the decrypt function job

--- src/invidious.cr
+++ src/invidious.cr
@@ -136,11 +136,6 @@ if CONFIG.feed_threads > 0
   Invidious::Jobs.register Invidious::Jobs::RefreshFeedsJob.new(PG_DB)
 end
 
-DECRYPT_FUNCTION = DecryptFunction.new(CONFIG.decrypt_polling)
-if CONFIG.decrypt_polling
-  Invidious::Jobs.register Invidious::Jobs::UpdateDecryptFunctionJob.new
-end
-
 if CONFIG.statistics_enabled
   Invidious::Jobs.register Invidious::Jobs::StatisticsRefreshJob.new(PG_DB, SOFTWARE)
 end
