4b3d5dc20fb096bfdaa2825062e6f058a8fd65a1
syeopite <syeopite@syeopite.dev>
2021-09-03 02:08:23 -0700

    Handle when YT returns EQUIRECTANGULAR

--- assets/js/player.js
+++ assets/js/player.js
@@ -151,7 +151,12 @@ player.on('error', function (event) {
 // Enable VR video support
 if (video_data.vr && video_data.params.vr_mode) {
     player.crossOrigin("anonymous")
-    player.vr({projection: "EAC"});
+    switch (video_data.projection_type) {
+        case "EQUIRECTANGULAR":
+            player.vr({projection: "equirectangular"});
+        default: // Should only be "MESH". But We'll use this as a fallback.
+            player.vr({projection: "EAC"});
+    }
 }
 
 // Add markers
