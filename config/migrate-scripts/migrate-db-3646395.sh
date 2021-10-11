df968d1919227949bcbeedb253d66ed0720b3d2a
JuniorJPDJ <github.com@juniorjpdj.pl>
2021-10-11 21:16:56 +0200

    variable postgres username and database name (#2451)

--- config/migrate-scripts/migrate-db-3646395.sh
+++ config/migrate-scripts/migrate-db-3646395.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-psql invidious kemal < config/sql/session_ids.sql
-psql invidious kemal -c "INSERT INTO session_ids (SELECT unnest(id), email, CURRENT_TIMESTAMP FROM users) ON CONFLICT (id) DO NOTHING"
-psql invidious kemal -c "ALTER TABLE users DROP COLUMN id"
+[ -z "$POSTGRES_USER" ] && POSTGRES_USER=kemal
+[ -z "$POSTGRES_DB" ] && POSTGRES_DB=invidious
+
+psql "$POSTGRES_DB" "$POSTGRES_USER" < config/sql/session_ids.sql
+psql "$POSTGRES_DB" "$POSTGRES_USER" -c "INSERT INTO session_ids (SELECT unnest(id), email, CURRENT_TIMESTAMP FROM users) ON CONFLICT (id) DO NOTHING"
+psql "$POSTGRES_DB" "$POSTGRES_USER" -c "ALTER TABLE users DROP COLUMN id"
