c5967ad572191ad7b99dec08111974b04dffc6d0
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2022-01-25 11:35:19 -0600

    will -> should
    Co-authored-by: Samantaz Fox <coding@samantaz.fr>

--- src/invidious/routes/search.cr
+++ src/invidious/routes/search.cr
@@ -56,7 +56,7 @@ module Invidious::Routes::Search
       begin
         search_query, count, videos, operators = process_search_query(query, page, user, region: region)
       rescue ex : ChannelSearchException
-        return error_template(404, "Unable to find channel with id of '#{HTML.escape(ex.channel)}'. Are you sure that's an actual channel id? It will look like 'UC4QobU6STFB0P71PMvOGN5A'.")
+        return error_template(404, "Unable to find channel with id of '#{HTML.escape(ex.channel)}'. Are you sure that's an actual channel id? It should look like 'UC4QobU6STFB0P71PMvOGN5A'.")
       rescue ex
         return error_template(500, ex)
       end
