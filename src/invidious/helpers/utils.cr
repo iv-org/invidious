6c122248f595a338e565bf73b1b6e5a2b761b894
Féry Mathieu (Mathius) <ferymathieuy@gmail.com>
2022-04-14 22:42:21 +0200

    Update regex reduce_uri utils
    Follow this comment :
    https://github.com/iv-org/invidious/pull/2936#discussion_r850712676

--- src/invidious/helpers/utils.cr
+++ src/invidious/helpers/utils.cr
@@ -367,7 +367,7 @@ def fetch_random_instance
 end
 
 def reduce_uri(uri : URI | String, max_length : Int32 = 50, suffix : String = "…") : String
-  str = uri.to_s.sub(/https?:\/\//, "")
+  str = uri.to_s.sub(/^https?:\/\//, "")
   if str.size > max_length
     str = "#{str[0, max_length]}#{suffix}"
   end
