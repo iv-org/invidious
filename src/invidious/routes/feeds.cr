f5fb4c6c64da58415dafba34087fa7dd9c11509a
Samantaz Fox <coding@samantaz.fr>
2022-05-01 21:10:43 +0200

    Apply 2859.diff

--- src/invidious/routes/feeds.cr
+++ src/invidious/routes/feeds.cr
@@ -182,6 +182,7 @@ module Invidious::Routes::Feeds
         paid:               false,
         premium:            false,
         premiere_timestamp: nil,
+        author_verified:    false, # 	¯\_(ツ)_/¯
       })
     end
 
