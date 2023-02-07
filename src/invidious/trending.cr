28424d0e881c8595bbc5797b9ef46e98103fe6d6
ChunkyProgrammer <78101139+ChunkyProgrammer@users.noreply.github.com>
2023-02-07 09:23:26 -0500

    Ignore casing for trending type in api

--- src/invidious/trending.cr
+++ src/invidious/trending.cr
@@ -4,11 +4,14 @@ def fetch_trending(trending_type, region, locale)
 
   plid = nil
 
-  if trending_type == "Music"
+  trending_type ||= "default"
+  trending_type = trending_type.downcase
+
+  if trending_type == "music"
     params = "4gINGgt5dG1hX2NoYXJ0cw%3D%3D"
-  elsif trending_type == "Gaming"
+  elsif trending_type == "gaming"
     params = "4gIcGhpnYW1pbmdfY29ycHVzX21vc3RfcG9wdWxhcg%3D%3D"
-  elsif trending_type == "Movies"
+  elsif trending_type == "movies"
     params = "4gIKGgh0cmFpbGVycw%3D%3D"
   else # Default
     params = ""
