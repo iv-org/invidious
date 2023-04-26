462609d90d38ec8e9aada1d700cfbca46e906552
Samantaz Fox <coding@samantaz.fr>
2023-04-26 22:30:13 +0200

    Utils: Create a function to append parameters to a base URL

--- src/invidious/http_server/utils.cr
+++ src/invidious/http_server/utils.cr
@@ -1,3 +1,5 @@
+require "uri"
+
 module Invidious::HttpServer
   module Utils
     extend self
@@ -15,6 +17,24 @@ module Invidious::HttpServer
       else
         return "#{url.request_target}?#{params}"
       end
+    end
+
+    def add_params_to_url(url : String | URI, params : URI::Params) : URI
+      url = URI.parse(url) if url.is_a?(String)
+
+      url_query = url.query || ""
+
+      # Append the parameters
+      url.query = String.build do |str|
+        if !url_query.empty?
+          str << url_query
+          str << '&'
+        end
+
+        str << params
+      end
+
+      return url
     end
   end
 end
