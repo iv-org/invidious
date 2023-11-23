46344653688f733c720fe87b7a17d6b56bb25554
Daniil Hikkamorii <zcy@inbox.lv>
2023-11-23 19:35:21 +0200

    Provide enchanced audio selection for user
    With this change, instdead of showing bitrate in bps to the user, user
    will see pure bitrate in kbps or codec and bitrate for when listening
    mode is used.

--- src/invidious/routes/api/manifest.cr
+++ src/invidious/routes/api/manifest.cr
@@ -68,7 +68,9 @@ module Invidious::Routes::API::Manifest
               # However, most players don't support auto quality switching, so we have to trick them
               # into providing a quality selector.
               # See https://github.com/iv-org/invidious/issues/3074 for more details.
-              xml.element("AdaptationSet", id: i, mimeType: mime_type, startWithSAP: 1, subsegmentAlignment: true, label: fmt["bitrate"].to_s + "k") do
+              bitrate = ( fmt["bitrate"].as_i / 1000 ).round(0).to_i.to_s + "k"
+
+              xml.element("AdaptationSet", id: i, mimeType: mime_type, startWithSAP: 1, subsegmentAlignment: true, label: bitrate) do
                 codecs = fmt["mimeType"].as_s.split("codecs=")[1].strip('"')
                 bandwidth = fmt["bitrate"].as_i
                 itag = fmt["itag"].as_i
