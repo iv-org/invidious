2d6748aae40a99cd0cc94c5542bb628bfd8afb6d
broquemonsieur <bracero-uplink.08@icloud.com>
2023-12-18 01:19:40 -0800

    Add migrate script

--- src/invidious.cr
+++ src/invidious.cr
@@ -138,6 +138,9 @@ LOGGER = Invidious::LogHandler.new(OUTPUT, CONFIG.log_level)
 # Check table integrity
 Invidious::Database.check_integrity(CONFIG)
 
+# Run migration
+Invidious::Database::Migrator.new(PG_DB).migrate
+
 {% if !flag?(:skip_videojs_download) %}
   # Resolve player dependencies. This is done at compile time.
   #
