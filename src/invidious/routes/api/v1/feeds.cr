5fa293541b05aa9fc0be404e5e872cb991cbe423
Richard Lora <nas.serverx0x@gmail.com>
2024-05-03 10:36:43 -0400

    fix(feeds.cr): http status code

--- src/invidious/routes/api/v1/feeds.cr
+++ src/invidious/routes/api/v1/feeds.cr
@@ -6,7 +6,7 @@ module Invidious::Routes::API::V1::Feeds
 
     if !CONFIG.trending_enabled
       error_message = {"error" => "Administrator has disabled this endpoint."}.to_json
-      haltf env, 400, error_message
+      haltf env, 403, error_message
     end
 
     region = env.params.query["region"]?
@@ -36,7 +36,7 @@ module Invidious::Routes::API::V1::Feeds
 
     if !CONFIG.popular_enabled
       error_message = {"error" => "Administrator has disabled this endpoint."}.to_json
-      haltf env, 400, error_message
+      haltf env, 403, error_message
     end
 
     JSON.build do |json|
