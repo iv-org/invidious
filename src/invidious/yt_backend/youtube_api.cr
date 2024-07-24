53223f99b03ac1a51cb35f7c33d4939083dc6f1a
Emilien Devos <4016501+unixfox@users.noreply.github.com>
2024-07-24 19:28:47 +0200

    Add ability to set po_token and visitordata ID

--- src/invidious/yt_backend/youtube_api.cr
+++ src/invidious/yt_backend/youtube_api.cr
@@ -320,6 +320,10 @@ module YoutubeAPI
       client_context["client"]["platform"] = platform
     end
 
+    if CONFIG.visitor_data.is_a?(String)
+      client_context["client"]["visitorData"] = CONFIG.visitor_data.as(String)
+    end
+
     return client_context
   end
 
@@ -467,6 +471,9 @@ module YoutubeAPI
           "html5Preference": "HTML5_PREF_WANTS",
         },
       },
+      "serviceIntegrityDimensions" => {
+        "poToken" => CONFIG.po_token,
+      },
     }
 
     # Append the additional parameters if those were provided
@@ -597,6 +604,10 @@ module YoutubeAPI
 
     if user_agent = client_config.user_agent
       headers["User-Agent"] = user_agent
+    end
+
+    if CONFIG.visitor_data.is_a?(String)
+      headers["X-Goog-Visitor-Id"] = CONFIG.visitor_data.as(String)
     end
 
     # Logging
