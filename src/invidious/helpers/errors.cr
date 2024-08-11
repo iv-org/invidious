824206612dd54e6a9c52e43a8f45d761a235a795
broquemonsieur <bracero-uplink.08@icloud.com>
2024-08-10 22:59:17 -0700

    Add dark light interoperability

--- src/invidious/helpers/errors.cr
+++ src/invidious/helpers/errors.cr
@@ -75,7 +75,8 @@ def error_template_helper(env : HTTP::Server::Context, status_code : Int32, exce
   # proposed above the error message, just worded differently.
   next_steps = ""
   unfound_tv_box_dark_theme = File.read("assets/404_tv_box_dark_theme.svg")
-  
+  unfound_tv_box_light_theme = File.read("assets/404_tv_box_light_theme.svg")
+
   return templated "error"
 end
 
@@ -87,6 +88,7 @@ def error_template_helper(env : HTTP::Server::Context, status_code : Int32, mess
 
   error_message = translate(locale, message)
   unfound_tv_box_dark_theme = File.read("assets/404_tv_box_dark_theme.svg")
+  unfound_tv_box_light_theme = File.read("assets/404_tv_box_light_theme.svg")
   next_steps = error_redirect_helper(env)
 
   return templated "error"
