b761ed3dcd8c96e61013eec75277397f12ae5056
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2024-10-15 17:54:52 -0400

    another fix

--- src/invidious/jobs/token_monitor.cr
+++ src/invidious/jobs/token_monitor.cr
@@ -6,7 +6,7 @@ class Invidious::Jobs::MonitorCfgTokensJob < Invidious::Jobs::BaseJob
         
       LOGGER.info("jobs: running MonitorCfgTokensJob job")
     
-      ReloadPOToken.get_tokens
+      ReloadToken.get_tokens
     
       LOGGER.info("jobs: MonitorCfgTokensJob: pot: " + ReloadTokens.pot.as(String))
       LOGGER.info("jobs: MonitorCfgTokensJob: vdata: " + ReloadTokens.vdata.as(String))
