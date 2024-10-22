d9b9cc0d7c44627654f426adc520efded40774f0
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2024-10-21 23:33:37 -0400

    forgot a file

---
+++ src/invidious/helpers/loadavg.cr
@@ -0,0 +1,23 @@
+class Invidious::Loadavg
+  
+  def self.get_loadavg
+    
+    str_loadavg = "error"
+
+    if CONFIG.loadavg_enabled
+          
+      # get the uptime
+      loadavg_cmd = "/usr/bin/cat /proc/loadavg  | awk -F'[ ]' '{print $1\" \"$2\" \"$3}'"
+      loadavg_args = ""
+      process = Process.new(loadavg_cmd, [loadavg_args], output: Process::Redirect::Pipe)
+      str_loadavg = process.output.gets_to_end
+      
+    else
+      str_loadavg = ""
+    end
+    
+    return str_loadavg
+
+  end
+  
+end
