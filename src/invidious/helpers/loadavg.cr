6b426c28861799c6a3d5d9f5e32818139a368a57
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2024-10-21 23:48:52 -0400

    test

--- src/invidious/helpers/loadavg.cr
+++ src/invidious/helpers/loadavg.cr
@@ -7,10 +7,12 @@ class Invidious::Loadavg
     if CONFIG.loadavg_enabled
           
       # get the uptime
-      loadavg_cmd = "/usr/bin/cat /proc/loadavg  | awk -F'[ ]' '{print $1\" \"$2\" \"$3}'"
-      loadavg_args = ""
-      process = Process.new(loadavg_cmd, [loadavg_args], output: Process::Redirect::Pipe)
-      str_loadavg = process.output.gets_to_end
+      #loadavg_cmd = "/usr/bin/cat /proc/loadavg  | awk -F'[ ]' '{print $1\" \"$2\" \"$3}'"
+      #loadavg_args = ""
+      #process = Process.new(loadavg_cmd, [loadavg_args], output: Process::Redirect::Pipe)
+      #str_loadavg = process.output.gets_to_end
+      
+      str_loadavg = "test"
       
     else
       str_loadavg = ""
