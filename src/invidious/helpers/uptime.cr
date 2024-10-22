cc4b3bf888096f1e41563761a369998af48200bd
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2024-10-21 22:20:20 -0400

    added config variable

--- src/invidious/helpers/uptime.cr
+++ src/invidious/helpers/uptime.cr
@@ -1,15 +1,20 @@
 class Invidious::Uptime
   
   def self.get_uptime
-
+    
     str_uptime = "error"
 
-    # get the uptime
-    uptime_cmd = "/usr/bin/uptime"
-    uptime_args = "-p"
-    process = Process.new(uptime_cmd, [uptime_args], output: Process::Redirect::Pipe)
-    
-    str_uptime = process.output.gets_to_end
+    if CONFIG.uptime_enabled
+          
+      # get the uptime
+      uptime_cmd = "/usr/bin/uptime"
+      uptime_args = "-p"
+      process = Process.new(uptime_cmd, [uptime_args], output: Process::Redirect::Pipe)
+      str_uptime = process.output.gets_to_end
+      
+    else
+      str_uptime = ""
+    end
     
     return str_uptime
 
