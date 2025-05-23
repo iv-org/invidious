4b37d47ebbc4d3a0a55c8febaca2b28a68e1d9b5
Fijxu <fijxu@nadeko.net>
2025-05-22 20:12:54 -0400

    Add missing xml.text on "title" element for channels RSS (#5320)

--- src/invidious/routes/feeds.cr
+++ src/invidious/routes/feeds.cr
@@ -202,7 +202,7 @@ module Invidious::Routes::Feeds
         xml.element("link", rel: "self", href: "#{HOST_URL}#{env.request.resource}")
         xml.element("id") { xml.text "yt:channel:#{ucid}" }
         xml.element("yt:channelId") { xml.text ucid }
-        xml.element("title") { author }
+        xml.element("title") { xml.text author }
         xml.element("link", rel: "alternate", href: "#{HOST_URL}/channel/#{ucid}")
 
         xml.element("author") do
