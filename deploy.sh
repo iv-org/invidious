9c0757a500831df4f0e810f98c4f00523a5ed01b
Ramon <theramon@gmail.com>
2025-05-28 17:11:05 +0200

    chore: Remove helper files

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-docker buildx build --platform=linux/x86_64 --tag=invidious . && docker tag invidious main.local:20000/invidious/invidious
-docker push main.local:20000/invidious/invidious
-docker rmi $(docker images main.local:20000/invidious/invidious -q) --force
-echo "Finished."
