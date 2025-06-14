65fee592344be7c2df4f2ddd82e2faa7bd05e97e
Emilien <4016501+unixfox@users.noreply.github.com>
2025-06-14 22:35:11 +0200

    fix formatting

--- src/invidious/routes/watch.cr
+++ src/invidious/routes/watch.cr
@@ -198,7 +198,7 @@ module Invidious::Routes::Watch
         uri =
           "#{companion.public_url.scheme}://#{companion.public_url.host}#{companion.public_url.port ? ":#{companion.public_url.port}" : ""}"
       end.join(" ")
-      
+
       if !invidious_companion_urls.empty?
         env.response.headers["Content-Security-Policy"] =
           env.response.headers["Content-Security-Policy"]
