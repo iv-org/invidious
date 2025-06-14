98d482b99f8f64c6d71cbcfccd9ecbeef05bb4dd
Emilien <4016501+unixfox@users.noreply.github.com>
2025-06-14 19:10:52 +0200

    fix csp + progress proxy + allow omit public_url

--- src/invidious/routes/companion.cr
+++ src/invidious/routes/companion.cr
@@ -1,22 +1,33 @@
 module Invidious::Routes::Companion
   # /companion
   def self.get_companion(env)
-    url = env.request.path.lchop("/companion")
+    url = env.request.path
+    if env.request.query
+      url += "?#{env.request.query}"
+    end
 
     begin
-      COMPANION_POOL.client &.get(url, env.request.header) do |resp|
-        return self.proxy_companion(env, resp)
+      COMPANION_POOL.client do |wrapper|
+        puts env.request.headers
+        wrapper.client.get(url, env.request.headers) do |resp|
+          return self.proxy_companion(env, resp)
+        end
       end
     rescue ex
     end
   end
 
   def self.options_companion(env)
-    url = env.request.path.lchop("/companion")
+    url = env.request.path
+    if env.request.query
+      url += "?#{env.request.query}"
+    end
 
     begin
-      COMPANION_POOL.client &.options(url, env.request.header) do |resp|
-        return self.proxy_companion(env, resp)
+      COMPANION_POOL.client do |wrapper|
+        wrapper.client.options(url, env.request.headers) do |resp|
+          return self.proxy_companion(env, resp)
+        end
       end
     rescue ex
     end
