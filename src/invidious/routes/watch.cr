9aaad689298e3629dabe84f13c57083aab768d28
Gus Libens <27970303+goestav@users.noreply.github.com>
2025-11-07 18:37:08 +0100

    refactor: prevent server from marking the video as "watched" when the page loads
    This will be handled at the front-end with JavaScript.

--- src/invidious/routes/watch.cr
+++ src/invidious/routes/watch.cr
@@ -67,10 +67,6 @@ module Invidious::Routes::Watch
     end
     env.params.query.delete_all("iv_load_policy")
 
-    if watched && preferences.watch_history
-      Invidious::Database::Users.mark_watched(user.as(User), id)
-    end
-
     if CONFIG.enable_user_notifications && notifications && notifications.includes? id
       Invidious::Database::Users.remove_notification(user.as(User), id)
       env.get("user").as(User).notifications.delete(id)
