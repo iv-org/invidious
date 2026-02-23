27d23ae51387ac4f5032efbc9a0016038a872c34
Fijxu <fijxu@nadeko.net>
2026-02-23 18:32:20 -0300

    rename author_handle to channel_handle

--- src/invidious/routes/api/v1/channels.cr
+++ src/invidious/routes/api/v1/channels.cr
@@ -48,7 +48,7 @@ module Invidious::Routes::API::V1::Channels
       # TODO: Refactor into `to_json` for InvidiousChannel
       json.object do
         json.field "author", channel.author
-        json.field "authorHandle", channel.author_handle
+        json.field "channelHandle", channel.channel_handle
         json.field "authorId", channel.ucid
         json.field "authorUrl", channel.author_url
 
