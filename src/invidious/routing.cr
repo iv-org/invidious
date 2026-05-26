edb3a0fc360914c705bb749f27d05f2323b7782e
Fijxu <fijxu@nadeko.net>
2026-05-26 18:13:51 -0400

    Add support for `/pl_c`  and `/tvfilm_banner` paths (thumbnails used in some playlists) (#5742)
    * feat: add support for /pl_c/ images
    
    This path can be found on Podcast images.
    
    * add support for /pl_c and /tvfilm_banner paths
    
    * add support for /pl_c and /tvfilm_banner paths 2
    
    * remove leftover comment

--- src/invidious/routing.cr
+++ src/invidious/routing.cr
@@ -222,6 +222,8 @@ module Invidious::Routing
     get "/s_p/:id/:name", Routes::Images, :s_p_image
     get "/yts/img/:name", Routes::Images, :yts_image
     get "/vi/:id/:name", Routes::Images, :thumbnails
+    get "/pl_c/:id/:name", Routes::Images, :pl_c_image
+    get "/tvfilm_banner/:id/:name", Routes::Images, :tvfilm_banner_image
   end
 
   def register_companion_routes
